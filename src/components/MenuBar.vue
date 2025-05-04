1474d877e4e7ee871a054dbf5f034ce830f516e3
James Spears <jamesspearsv@gmail.com>
2025-05-04 08:25:32 -0400

    Update MenuBar components

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const open = ref(false)
+</script>
+
 <template>
-  <nav>
-    <slot />
+  <nav :class="{ open: open }">
+    <menu @click="() => (open = false)">
+      <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
+    </menu>
+    <button @click="() => (open = !open)">
+      <div>Menu</div>
+      <div class="menu-icon">▲</div>
+    </button>
   </nav>
 </template>
 
@@ -10,6 +22,51 @@ nav {
   bottom: 0;
   left: 0;
   right: 0;
-  background-color: var(--c-secondary);
+  top: 92dvh;
+  padding: var(--sp-sm);
+  background-color: var(--c-bg-alt);
+  border-top: solid 1px var(--c-secondary);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  transition-property: top border-top;
+  transition-duration: 200ms;
+  transition-timing-function: ease-in-out;
+}
+
+nav.open {
+  top: 0;
+  border: none;
+}
+
+button {
+  background-color: inherit;
+  display: flex;
+  align-items: center;
+  gap: var(--sp-sm);
+}
+
+.menu-icon {
+  transition: transform 200ms ease-in-out;
+}
+
+nav.open .menu-icon {
+  transform: rotate(180deg);
+}
+
+menu {
+  height: 0;
+  overflow-y: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: var(--sp-xs);
+  transition: height 200ms ease-in-out;
+}
+
+nav.open menu {
+  height: 90dvh;
 }
 </style>
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
+import SignupView from '@/views/SignupView.vue'
 import { signout } from '@/lib/auth'
 import { pb } from '@/lib/pb'
-import SignupView from '@/views/SignupView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
