6b752a84020398e45d30f47f957103b797793ca7
James Spears <jamesspearsv@gmail.com>
2025-05-07 22:36:15 -0400

    Implement habit pattern gradient logic

--- src/views/AppViews/HabitPatternView.vue
+++ src/views/AppViews/HabitPatternView.vue
@@ -1,15 +1,38 @@
 <script setup lang="ts">
 import { fetchActivities } from '@/lib/actions'
 import type { MappedActivity } from '@/lib/types'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 const activities = ref<MappedActivity[] | null>()
+const colors = ref<{ [key: string]: string[] }>({})
+const clr = computed(() => {
+  const c = { ...colors.value }
+  Object.keys(c).forEach((key) => {
+    const length = c[key].length
+    // TODO: compute average colors from array of hex codes
+    c[key] = ['string']
+  })
+  return c
+})
 
 onMounted(async () => {
   const result = await fetchActivities()
-  if (result.success) activities.value = result.data
+  if (result.success) {
+    activities.value = result.data
+    activities.value.forEach((activity) => {
+      const date = new Date(activity.date).toLocaleString().split(',')[0]
+      if (!colors.value[date]) {
+        colors.value = { ...colors.value, [date]: [activity.color.slice(1)] }
+      } else {
+        colors.value = { ...colors.value, [date]: [...colors.value[date], activity.color.slice(1)] }
+      }
+    })
+    console.log(result.data)
+  }
 })
+console.log(colors)
 </script>
 <template>
+  <div>{{}}</div>
   <h1>Habit Pattern!</h1>
   <section>
     <article
