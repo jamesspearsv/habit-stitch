c923ca3bffa304391cddcc5d9743670b986293d5
James Spears <jamesspearsv@gmail.com>
2025-08-30 00:16:31 -0400

    Add create table query in worker script

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,29 @@
 import { Hono } from 'hono'
-const app = new Hono()
+
+type Bindings = {
+  DB: D1Database
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 // Cloudflare D1 and Hono guides
 // https://developers.cloudflare.com/d1/worker-api/d1-database/
 // https://hono.dev/docs/getting-started/cloudflare-workers#bindings
+// https://developers.cloudflare.com/d1/examples/d1-and-hono/
+
+app.get('/api/createTable', async (c) => {
+  console.log(c.env.DB)
+  const stmt = `CREATE TABLE IF NOT EXISTS habits (name TEXT, completion_goal INTEGER);`
+  const query = c.env.DB.prepare(stmt)
+
+  try {
+    await query.run()
+    return c.json({ message: 'Table created' })
+  } catch (error) {
+    console.error(error)
+    return c.json({ message: 'Unable to create table' })
+  }
+})
 
 app.get('/api', (c) => c.json({ message: 'Hello from Cloudflare Workers!' }))
 
--- src/views/WorkerView.vue
+++ src/views/WorkerView.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-async function handleClick() {
-  const res = await fetch('/api')
+async function handleClick(route: string) {
+  const res = await fetch(route)
   const json = await res.json()
   alert(JSON.stringify(json))
 }
 </script>
 <template>
   <main>
-    <button @click="handleClick">Access Basic Worker Code</button>
+    <button @click="() => handleClick('/api')">Access Basic Worker Code</button>
+    <button @click="() => handleClick('/api/createTable')">Create Table</button>
   </main>
 </template>
 <style>
@@ -16,5 +17,6 @@ main {
   flex-direction: column;
   align-items: center;
   padding: 1rem;
+  gap: 1rem;
 }
 </style>
