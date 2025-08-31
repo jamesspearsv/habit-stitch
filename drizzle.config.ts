70e81cd941a78710ef50077ac1dbee313f0a7aa5
James Spears <jamesspearsv@gmail.com>
2025-08-31 08:05:09 -0400

    Update drizzle config for local development

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'drizzle-kit'
+import path from 'path'
+import fs from 'fs'
 
 /*
  * Drizzle workflow
@@ -11,18 +13,43 @@ import { defineConfig } from 'drizzle-kit'
 
 // TODO: Add dynamic config script to handle local development.
 
-function findLocalD1Database() {}
+function findLocalD1DB() {
+  try {
+    const basePath = path.resolve('.wrangler')
+    const dbFile = fs
+      .readdirSync(basePath, { encoding: 'utf-8', recursive: true })
+      .find((f) => f.endsWith('.sqlite'))
+
+    if (!dbFile) {
+      throw new Error(`.sqlite file not found in ${basePath}`)
+    }
+
+    console.log(basePath + dbFile)
+    return path.resolve(basePath, dbFile)
+  } catch (error) {
+    if (error instanceof Error) console.log(`Error: ${error.message}`)
+  }
+}
 
 // if env === dev use local sqlite database
 // else use remote D1 instance
 
 export default defineConfig({
   dialect: 'sqlite',
-  driver: 'd1-http',
   schema: './server/schema.ts',
-  dbCredentials: {
-    accountId: process.env.CF_ACCOUNT_ID!,
-    databaseId: process.env.CF_DATABASE_ID!,
-    token: process.env.CF_D1_TOKEN!,
-  },
+
+  ...(process.env.NODE_ENV === 'prod'
+    ? {
+        driver: 'd1-http',
+        dbCredentials: {
+          accountId: process.env.CF_ACCOUNT_ID!,
+          databaseId: process.env.CF_DATABASE_ID!,
+          token: process.env.CF_D1_TOKEN!,
+        },
+      }
+    : {
+        dbCredentials: {
+          url: findLocalD1DB()!,
+        },
+      }),
 })
